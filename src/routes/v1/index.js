39736474db318eac7266bb158b8cbf3b29cf4dc6
SubratYeeshu <shubrat.parth@gmail.com>
2023-09-17 19:00:08 +0530

    Added getAllCities API

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,7 @@
 // All v1 routes will be prefixed with /v1 and will be handled by v1ApiRoutes
 
+// Request flow : Routes -> Middlewares -> Controllers -> Services -> Repository -> Models -> Database
+
 const express = require('express');
 const CityController = require('../../controllers/city-controller');
 const router = express.Router();
@@ -7,6 +9,7 @@ const router = express.Router();
 router.post('/city', CityController.create);
 router.delete('/city/:id', CityController.destroy);
 router.get('/city/:id', CityController.get);
+router.get('/city', CityController.getAll);
 router.patch('/city/:id', CityController.update);
 
 
