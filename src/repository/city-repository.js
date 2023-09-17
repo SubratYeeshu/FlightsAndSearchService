9aa31e615c25d7f98755c7aa52cc68e9a2403a70
SubratYeeshu <shubrat.parth@gmail.com>
2023-09-17 10:20:11 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -4,28 +4,58 @@
 // Main purpose of this file is to perform CRUD operations on the City table, we can also do this in the controller but it is not a good practice
 // There can be many types of errors like client side errors, server side errors, database errors, etc. So, we can segregate the error handling logic in different folders.
 // Work of repository layer to interact with models and database
+// Now we will use cuty repository in the city controller to perform CRUD operations on the City table and create APIs.
 
-const {City} = require('../models/index');
+const { City } = require('../models/index');
 
-class CityRepository{
-    async createCity({name}){
-        try{
-            const city = await City.create({name});
+class CityRepository {
+    async createCity({ name }) {
+        try {
+            const city = await City.create({
+                name
+            });
             return city;
-        }catch(error){
-            throw {error};
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw { error };
         }
     }
 
-    async deleteCity(cityId){
-        try{
+    async deleteCity(cityId) {
+        try {
             await City.destroy({
-                where:{
-                    id : cityId
+                where: {
+                    id: cityId
                 }
             });
+            return true;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw { error };
+        }
+    }
+
+    async updateCity(cityId, data) { 
+        try{
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
         }catch(error){
-            throw {error};
+            console.log("Something went wrong in the repository layer");
+            throw { error };
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw { error };
         }
     }
 }
