9031a252a890010806f3ad79827688510c593001
SubratYeeshu <shubrat.parth@gmail.com>
2023-09-19 15:12:11 +0530

    Added Error Code ENUMS

--- src/middlewares/flight-middlewares.js
+++ src/middlewares/flight-middlewares.js
@@ -1,6 +1,9 @@
 // Middlewares are just functions that have access to the request and response objects and the next middlware, they are specially useful for validations, authentication, authorization, etc. In our case we will be using them for validation and authentication.
 // We should validate the request body before sending it to the controller, so we will create a middleware for that.
 // Middleware filters out the properties which are not abiding the contract.
+
+const {ClientErrorCodes} = require('../utils/error-codes'); 
+
 const validateCreateFlight = (req, res, next) => {
     if(
         !req.body.flightNumber ||
@@ -12,7 +15,7 @@ const validateCreateFlight = (req, res, next) => {
         !req.body.price
     ){
         // If any of the body parameter is missing then we will go inside th if block
-        return res.status(400).json({
+        return res.status(ClientErrorCodes.BAD_REQUEST).json({
             data: {},
             success: false,
             message: 'Invalid request body for create flight',
---
+++ src/utils/error-codes.js
@@ -0,0 +1,23 @@
+// Code for error handling, Object.freeze() is used to make the object immutable.
+
+const ClientErrorCodes = Object.freeze({
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    NOT_FOUND: 404,
+});
+
+const ServerErrorCodes = Object.freeze({
+    INTERNAL_SERVER_ERROR: 500,
+    NOT_IMPLEMENTED: 501,
+});
+
+const SuccessCodes = Object.freeze({
+    OK: 200,
+    CREATED: 201,
+});
+
+module.exports = {
+    ClientErrorCodes,
+    ServerErrorCodes,
+    SuccessCodes
+}
